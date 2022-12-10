d0fbdcf99501b3213d8cfa8a8cbd4440c783e29c
Guilherme Gasparotti Cassettari <guilherme.cassettari@webjump.com.br>
2022-12-09 23:14:44 -0300

    add animation

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,6 +8,8 @@ import AnimatedLottieView from 'lottie-react-native'
 
 import * as S from './styles'
 import CardShimmer from "../../components/CardShimmer";
+import EnterAnimation from "../../components/animations/EnterAnimation";
+import FadeAnimation from "../../components/animations/FadeAnimation";
 
 const windowWidth = Dimensions.get('window').width;
 const Home = () => {
@@ -26,40 +28,52 @@ const Home = () => {
     }, [loading])
     return (
         <S.Wrapper>
-            <S.TextPokeSize>
-                More than 1249 Pokemons for you choose your favorite
-            </S.TextPokeSize>
-            <SearchBar />
-            {loading && showShimmer ? (
-                <CardShimmer />
-            ) : (
-                <FlatList
-                    numColumns={2}
-                    columnWrapperStyle={{
-                        justifyContent: 'space-evenly',
-                        width: Math.floor(windowWidth - 30)
-                    }}
-                    onEndReached={fetchMorePokemons}
-                    onEndReachedThreshold={0.1}
-                    data={card}
-                    keyExtractor={pokemon => pokemon.id.toString()}
-                    renderItem={({ item: pokemon }) => (
-                        <PokeCard 
-                            pokemon={pokemon}
-                            delay={(pokemon.enterAnimation)}
-                        />
-                    )}
-                />
-            )}
-            {(searchLoader || (!showShimmer && loading)) && (
-                <S.WrapperAnimation accessibilityHint="Await for pokemon initial loader">
-                    <AnimatedLottieView
-                        autoPlay={true}
-                        source={require('../../../assets/loader/pokeLoader.json')}
-                        loop={true}
+            <FadeAnimation
+                duration={800}
+            >
+                <EnterAnimation
+                    duration={600}
+                    delay={200}
+                >
+                    <S.TextPokeSize>
+                        More than 1249 Pokemons for you choose your favorite
+                    </S.TextPokeSize>
+                </EnterAnimation>
+                <EnterAnimation
+                duration={600}>
+                <SearchBar />
+                </EnterAnimation>
+                {loading && showShimmer ? (
+                    <CardShimmer />
+                ) : (
+                    <FlatList
+                        numColumns={2}
+                        columnWrapperStyle={{
+                            justifyContent: 'space-evenly',
+                            width: Math.floor(windowWidth - 30)
+                        }}
+                        onEndReached={fetchMorePokemons}
+                        onEndReachedThreshold={0.1}
+                        data={card}
+                        keyExtractor={pokemon => pokemon.id.toString()}
+                        renderItem={({ item: pokemon }) => (
+                            <PokeCard
+                                pokemon={pokemon}
+                                delay={(pokemon.enterAnimation)}
+                            />
+                        )}
                     />
-                </S.WrapperAnimation>
-            )}
+                )}
+                {(searchLoader || (!showShimmer && loading)) && (
+                    <S.WrapperAnimation accessibilityHint="Await for pokemon initial loader">
+                        <AnimatedLottieView
+                            autoPlay={true}
+                            source={require('../../../assets/loader/pokeLoader.json')}
+                            loop={true}
+                        />
+                    </S.WrapperAnimation>
+                )}
+            </FadeAnimation>
         </S.Wrapper>
     )
 }
